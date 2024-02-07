82e570cdf4b17f61d39496316c17eb0c2c8be021
patrickskipworth <pskipworth@fastmail.com>
2024-02-07 11:35:34 +0000

    Stuck working with json

---
+++ tests/test_time_error.py
@@ -0,0 +1,15 @@
+import time
+from unittest.mock import Mock
+from lib.time_error import TimeError
+
+def test_returns_error():
+    requester_mock = Mock(name='requester')
+    response_mock = Mock(name='response')
+
+    requester_mock.get.return_value = response_mock
+
+    response_mock.json.return_value = time.time() - 10
+
+    time_error = TimeError(requester_mock)
+    result = time_error.error()
+    assert result == 10
